14d70abfe3f9ccbe3d92d089b38013a22a789a59
Yusufdmrc <90138891+Yusufdmrc@users.noreply.github.com>
2021-11-23 16:22:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Customer> customers=new ArrayList<Customer>();
+
+        customers.add(new Customer(1,"Engin","Demiroğ"));
+        customers.add(new Customer(1,"Derin","Demiroğ"));
+        customers.add(new Customer(1,"Salih","Demiroğ"));
+
+        for (Customer customer:customers){
+            System.out.println(customer.firstName);
+        }
+    }
+}
